f96d4c778282341774c72822767673da88c7d9f6
Anchal Shukla <shuklaanchal532@gmail.com>
2025-04-26 20:10:14 +0530

    assignment

--- Answer16.java
+++ Answer16.java
@@ -1,3 +1,24 @@
+//majority element in an array that appears more than n/2 times
+
 public class Answer16 {
-    
+    //TC:O(N^2) SC:O(1)
+    public static int majorityElement(int[] nums){
+        int n=nums.length;
+        for(int i=0; i<n; i++){
+            int count=0;
+            for(int j=0; j<n; j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count>n/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums={3,2,3};
+        System.out.println(majorityElement(nums));
+    }
 }
---
+++ Answer17.java
@@ -0,0 +1,4 @@
+//Calculate how much water can be trapped between the bars of a histogram.
+public class Answer17 {
+    
+}
