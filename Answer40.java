25781f9914682c002baae40d8e896d7ae7bf06ce
Anchal Shukla <shuklaanchal532@gmail.com>
2025-04-28 14:53:19 +0530

    assignment

--- Answer40.java
+++ Answer40.java
@@ -1,30 +1,38 @@
 //find the element with maximum frequency in an array.
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Answer40 {
     //TC:O(N) SC:O(N)
     public static int maxFrequency(int[] nums){
-        HashMap<Integer,Integer> h=new HashMap<>();
-        for(int num:nums){
-            if(h.containsKey(num)){
-                h.put(num, h.get(num)+1);
-            }else{
-                h.put(num,1);
+        // HashMap<Integer,Integer> h=new HashMap<>();
+        // for(int num:nums){
+        //     if(h.containsKey(num)){
+        //         h.put(num, h.get(num)+1);
+        //     }else{
+        //         h.put(num,1);
+        //     }
+        // }
+       HashMap<Integer, Integer> h = new HashMap<>();
+        
+        for (int num : nums) {
+            h.put(num, h.getOrDefault(num, 0) + 1);
+        }
+
+        int maxFreq = 0;
+        int maxElement = -1;
+
+        for (Map.Entry<Integer, Integer> entry : h.entrySet()) {
+            if (entry.getValue() > maxFreq) {
+                maxFreq = entry.getValue();
+                maxElement = entry.getKey();
             }
         }
-        int max=0;
-        for(int val:h.values()){
-            max=Math.max(val, max);
-        }
-        int res=0;
-        for(int frq:h.values()){
-            if(frq==max){
-                res+=frq;
-            }
-        }
-        return res;
+
+        return maxElement;
     }
+    
     public static void main(String[] args) {
         int[] nums={1,2,2,3,1,4};
         System.out.println(maxFrequency(nums));
